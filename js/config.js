44954f369076140f9bec259642bdb4bc1fa0f58a
APrdl <98409471+APrdl@users.noreply.github.com>
2022-01-31 18:12:20 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-3_hNrfCOoCX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2qou6c6l04k0mfh2ba60rrdsgk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
